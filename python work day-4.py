8fe4b4382eda0d2923df96eda3fa60b6e6543c0c
amit kumar <amitkumaramit118589@gmail.com>
2024-09-27 12:32:23 +0530

    python work day-4.py

---
+++ python work day-4.py
@@ -0,0 +1,14 @@
+s = {"Mon", "Tue", "wed"}
+print(type(s))
+
+s.add("THru")
+print(s)
+
+s.update(["Fri"])
+print(s)
+
+s.discard("Tue")
+print(s)
+
+D = {101: "CE", 102: "ME", 105: "CSE"}
+print(type(D))
